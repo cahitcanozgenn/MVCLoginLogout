e27fd380d3bf8d55e2ab4eb8767ad04b1ab0026b
Cahit Can ÖZGEN <75495961+cahitcanozgenn@users.noreply.github.com>
2022-07-27 04:19:26 -0700

    MVCLoginLogout

---
+++ MVCLogınLogout/MVCLogınLogout/Controllers/SecurityController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using MVCLogınLogout.Models.Entity;
+namespace MVCLogınLogout.Controllers
+{
+   
+    public class SecurityController : Controller
+    {
+        userEntities userEntities = new userEntities();
+        // GET: Security
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Index(users users)
+        {
+            var value = userEntities.users.FirstOrDefault(x => x.usersUserName == users.usersUserName && x.usersPassword == users.usersPassword);
+            if (value != null)
+            {
+                FormsAuthentication.SetAuthCookie(users.usersUserName, false);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Mesaj = "Kullanıcı Adı Ya Da Şifre Hatalı";
+                return View();
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ MVCLogınLogout/MVCLogınLogout/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCLogınLogout.MvcApplication" Language="C#" %>
